536d89b7ffdec0b6759773d5bfec5e41360db428
DonCorrupto <georgealzate18@hotmail.com>
2022-10-05 12:22:28 -0500

    Smoke Test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,15 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2022": true,
-		"node": true,
-		"jest": true,
-		"worker": true
-	},
-	"extends": "eslint:recommended",
-	"overrides": [
-	],
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"windows"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
-};
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
