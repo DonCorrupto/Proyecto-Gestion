6f98acb5343b1b3258f5c8cbc9ecfd81bf977bbb
J_Alzate <georgealzate18@hotmail.com>
2022-10-05 00:07:34 -0500

    Implementando Smoke Test. Version CI

--- web/index.js
+++ web/index.js
@@ -521,6 +521,7 @@ async function cuentaNueva() {
 async function btniniciarSesion() {
   const email = document.getElementById("singemail").value;
   const password = document.getElementById("singpassword").value;
+  var boolean = false;
 
   auth.signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
