52387a3394192a668cc9aa362188199a28fa8c3b
J_Alzate <georgealzate18@hotmail.com>
2022-10-05 19:13:48 -0500

    Lint Arreglado

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 module.exports = {
     "env": {
-        "commonjs": true,
         "es2021": true,
         "node": true
     },
     "overrides": [
     ],
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
     "rules": {
     }
